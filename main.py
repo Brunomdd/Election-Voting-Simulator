81b76799d4c5940609956172721cdb3fa4c7e832
Brunomdd <brunnodiniiz@gmail.com>
2026-06-12 12:16:29 -0300

    fix: corrige cálculo de percentual dos votos

--- main.py
+++ main.py
@@ -13,10 +13,10 @@ def registrar_voto(votos):
 
 def mostrar(votos):
     c = 0
-    cabecalho("MOSTRAR CANDIDATOS")
     if not votos:
         print("Não há nada para mostrar!")
         return
+    cabecalho("MOSTRAR CANDIDATOS")
     for canditado,valor in votos.items():
         c+=1
         print(f"{c} - {canditado} - {valor}")
@@ -31,16 +31,28 @@ def pessoa_mais_votada(votos):
     if not votos:
         print('Nenhum voto foi registrado no momento!')
         return
+    cabecalho("Verificar Candidato mais votado")
     mais_votado = 0
-    porcentagem = 0
     maior_frequencia = None
     for chave,candidato in votos.items():
         if candidato > mais_votado:
-            mais_votado = candidato
+            qtd_votos = candidato
             maior_frequencia = chave
-    return {'candidato_maior_voto':maior_frequencia,
-            'QTD_votos':mais_votado
-            }
+    print(f"candidato com mais voto: {maior_frequencia} - Quantidade: {qtd_votos} votos\n")
+
+def mostrar_percentual(votos):
+    if not votos:
+        print('Não há candidados para mostrar no momemento!')
+        return
+    cabecalho("Mostrar percentual de candidatos mais votados")
+    total_votos = 0
+    for candidato,votos_recebidos in votos.items():
+        total_votos += votos_recebidos
+    
+    for candidato,votos_recebidos in votos.items():
+        percentual = (votos_recebidos/total_votos)* 100
+        print(f'{candidato} - {percentual:.2f}%')
+    
 
 def main():
     votos = {}
@@ -48,13 +60,16 @@ def main():
         cabecalho("VOTING SIMULATOR")
         menu(['Registrar Voto',
               'Ver Resultado',
-              'Ver o mais votado'])
+              'Ver o mais votado',
+              'ver percentual de votos'])
         opc = leia_int('Escolha uma opção ')
         if opc == 1:
             registrar_voto(votos)
         elif opc == 2:
             mostrar(votos)
         elif opc == 3:
-            res = pessoa_mais_votada(votos)
-            print(res)       
+            pessoa_mais_votada(votos)
+               
+        elif opc == 4:
+            mostrar_percentual(votos) 
 main()
